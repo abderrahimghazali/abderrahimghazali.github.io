b1930b87d29e7f48b45aecab165e87232721225b
Abderrahim GHAZALI <abderrahim.ghazali@smile.fr>
2019-07-03 17:41:57 +0200

    Updates

---
+++ source/_layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <title>Jigsaw – Static Sites for Laravel Developers</title>
+
+        <link rel="stylesheet" href="{{ $base }}/css/main.css">
+    </head>
+    <body>
+          <div class="flex-center position-ref full-height">
+              @yield('content')
+          </div>
+    </body>
+</html>
---
+++ source/index.blade.php
@@ -0,0 +1,15 @@
+@extends('_layouts.master')
+
+@section('content')
+    <div class="content">
+        <div class="m-b-md">
+            <h1 class="title">Jigsaw</h1>
+
+            <p>Static Sites for Laravel Developers</p>
+        </div>
+
+        <div class="links">
+            <a href="{{ $base }}/about">About</a>
+        </div>
+    </div>
+@endsection
