5ff52a357b0ab5973497373b9fc658b2fdf0cd1e
Abderrahim GHAZALI <ghazalI.abderrahim1@gmail.com>
2025-08-16 15:12:43 +0200

    about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { useEffect } from 'react'
 import Home from './Home'
 import About from './About'
 
 function App() {
-  useEffect(() => {
-    // Handle GitHub Pages SPA redirect
-    const query = window.location.search
-    if (query.includes('/?/')) {
-      const route = query.replace('/?/', '').replace(/&/g, '/')
-      window.history.replaceState(null, '', `/${route}`)
-    }
-  }, [])
-
   return (
     <Router>
       <Routes>
